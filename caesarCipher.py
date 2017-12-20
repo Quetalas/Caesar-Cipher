20e9377f2ca2a3da6541a498d570b44ef0a933e2
Quetalas <evgeny.kruzhckov@yandex.ru>
2017-12-20 22:46:38 +0300

    create funtion for hacking by brute forse

--- caesarCipher.py
+++ caesarCipher.py
@@ -24,6 +24,10 @@ def decrypt(mess, encryptionKey):
     for letter in mess:
         if letter in alphabetToDigit:
             num  = alphabetToDigit[letter] - encryptionKey
+            if num > 0:
+                num %= len(digitToAlphabet)
+            else:
+                num = -(-num % len(digitToAlphabet))
             if num >= 0:
                 decryptedText.append(digitToAlphabet[num])
             else:
@@ -33,8 +37,17 @@ def decrypt(mess, encryptionKey):
     return ''.join(decryptedText)
 
 
+def brute_force_hack(mess, rang = (-100, 100)):
+    res = []
+    for i in range(rang[0],rang[1]):
+        res.append(decrypt(mess, i))
+    return res
+
+
 if __name__ == '__main__':
     print('ABCXYZaz ->',encrypt('ABCXYZaz', 1), 'key = 1')
     print(decrypt(encrypt('ABCXYZaz', 1), 1), ' <- Decrypt , key = 1')
     print('The secret password is Rosebud ->', encrypt('The secret password is Rosebud', 8), 'key = 8')
     print(decrypt(encrypt('The secret password is Rosebud', 8), 8), ' <- Decrypt , key = 8')
+    hacked_text = brute_force_hack(encrypt('The secret password is Rosebud', 8))
+    print(hacked_text)
